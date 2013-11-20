8847f692a883d0ba11a14d4f353ca3d25300946c
Maxim Polishchuck <mpolishchuck@gmail.com>
2013-11-20 10:23:59 +0200

    Added DoctrineFixturesBundle and person data loader

---
+++ src/PaulMaxwell/StudentsAccountingBundle/DataFixtures/ORM/LoadPersonData.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace PaulMaxwell\StudentsAccountingBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use PaulMaxwell\StudentsAccountingBundle\Entity\Person;
+use Symfony\Component\Yaml\Yaml;
+
+class LoadPersonData implements FixtureInterface
+{
+
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param ObjectManager $manager
+     */
+    function load(ObjectManager $manager)
+    {
+        $personData = Yaml::parse(__DIR__ . '/data/persondata.yml');
+
+        for ($i = 0; $i < 50; $i++) {
+            $nameLast = $personData['last'][mt_rand(0, count($personData['last']) - 1)];
+            if (mt_rand(0, 10) > 5) {
+                $nameFirst = $personData['first']['male'][mt_rand(0, count($personData['first']['male']) - 1)];
+                $namePatronymic = $personData['patronymic']['male'][mt_rand(0, count($personData['patronymic']['male']) - 1)];
+            } else {
+                $nameFirst = $personData['first']['female'][mt_rand(0, count($personData['first']['female']) - 1)];
+                $namePatronymic = $personData['patronymic']['female'][mt_rand(0, count($personData['patronymic']['female']) - 1)];
+            }
+
+            $person = new Person();
+            $person->setNameLast($nameLast);
+            $person->setNameFirst($nameFirst);
+            $person->setNamePatronymic($namePatronymic);
+            $dateOfBirth = new \DateTime();
+            $dateOfBirth->setDate(mt_rand(1950, 2000), mt_rand(1,12), mt_rand(1,25));
+            $person->setDateOfBirth($dateOfBirth);
+
+            $manager->persist($person);
+        }
+
+        $manager->flush();
+    }
+
+    /**
+     * @return int
+     */
+    function getOrder()
+    {
+        return 1;
+    }
+}
